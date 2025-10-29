c2ca7bdf829d1fcd3050776315e372217731f933
Ahmed Nagi <ahmednagymahdyfrg252@gmail.com>
2025-10-29 11:10:43 +0300

    first commit

--- app/src/main/java/com/lol/ushop/ui/screens/category_screen/CategoriesScreen.kt
+++ app/src/main/java/com/lol/ushop/ui/screens/category_screen/CategoriesScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
@@ -96,6 +97,7 @@ fun CategoriesScreen(
             elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
         ) {
           Column(
+              modifier = Modifier.fillMaxSize().padding(8.dp),
               verticalArrangement = Arrangement.Center,
               horizontalAlignment = Alignment.CenterHorizontally,
           ) {
